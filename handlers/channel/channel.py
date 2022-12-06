29e781582cb276ae4ed197f723ad5494fa11d887
Artem <mrarlians@gmail.com>
2022-12-06 14:48:57 +0600

    fix

--- handlers/channel/channel.py
+++ handlers/channel/channel.py
@@ -1,7 +1,7 @@
 import typing
 import os
 
-from aiogram import types
+from aiogram import types, exceptions
 
 from main import bot
 from keyboard import inline
@@ -15,4 +15,12 @@ async def user_join(event: typing.Union[types.Message, types.ChatJoinRequest]):
         await event.approve()
 
     file = types.InputFile(os.path.join(os.path.abspath('.'), 'static', 'picture.jpg'))
-    await bot.send_photo(event.from_user.id, file, texts.message, reply_markup=inline.abount_menu)
+    try:
+        await bot.send_photo(
+            chat_id=event.from_user.id,
+            photo=file,
+            caption=texts.message,
+            reply_markup=inline.abount_menu
+        )
+    except (exceptions.CantTalkWithBots, exceptions.CantInitiateConversation):
+        pass
