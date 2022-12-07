72b574db3d7b2c56a089788446a7db5b233f44a1
Artem <mrarlians@gmail.com>
2022-12-07 06:52:53 +0600

    fix

--- handlers/commands/start.py
+++ handlers/commands/start.py
@@ -1,9 +1,15 @@
 from aiogram import types
 
+from database import DataBase, models
+from modules import Settings
 from keyboard import reply
 
 import texts
 
 
+db = DataBase(Settings.DatabaseUrl)
+
+
 async def starting(message: types.Message):
+    db.add(models.User, 'id', id=message.chat.id)
     await message.answer(texts.start_message, reply_markup=reply.main_menu)
